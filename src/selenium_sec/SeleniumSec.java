cedf363529bc92d11047a4924cb9665260716473
KOJI SUZUKI <pocaraxx@gmail.com>
2019-07-03 01:41:54 +0900

    temp

--- src/selenium_sec/SeleniumSec.java
+++ src/selenium_sec/SeleniumSec.java
@@ -1,8 +1,10 @@
 package selenium_sec;
 
-import org.openqa.selenium.By;
+import java.io.File;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class SeleniumSec {
@@ -12,23 +14,46 @@ public class SeleniumSec {
 		System.out.println("gagagaDD");
 
 
-	      //System.setProperty("webdriver.chrome.driver", "./exe/chromedriver.exe");
+
+
+
+
+
+		System.setProperty("webdriver.firefox.profile", "default-release");
+
+
+
+
+		//File profileDir = new File(path);
+		//FirefoxProfile profile = new FirefoxProfile(profileDir);
+		//WebDriver driver = new FirefoxDriver(profile);
+
+
 	      System.setProperty("webdriver.gecko.driver", "./exe/geckodriver.exe");
 	      DesiredCapabilities cap = DesiredCapabilities.firefox();
 			cap.setCapability("marionette", true);
+
+			String profileDir = "C:\\Users\\kouji\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\msf4k3yi.kouji";
+
+			FirefoxProfile fp = new FirefoxProfile(new File(profileDir));
+			cap.setCapability(FirefoxDriver.PROFILE, fp);
+
+
 			WebDriver driver = new FirefoxDriver(cap);
-			driver.navigate().to("http://www.google.com");
-			driver.findElement(By.id("lst-ib")).sendKeys("Selenium3");
-			driver.findElement(By.name("btnK")).click();
-			if(driver!=null) {
-				driver.close();
-			}
+			driver.navigate().to("about:addons");
+			//driver.findElement(By.id("lst-ib")).sendKeys("Selenium3");
+			//driver.findElement(By.name("btnK")).click();
+			//if(driver!=null) {
+				//driver.close();
+			//}
 
 
 
 
 
 /*
+ *
+ 		  	      //System.setProperty("webdriver.chrome.driver", "./exe/chromedriver.exe");
 	      WebDriver driver = new ChromeDriver();
 	      driver.get("http://www.google.com/xhtml");
 	      //Thread.sleep(5000);  // Let the user actually see something!
